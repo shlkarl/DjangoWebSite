35c36b4766ab241c001f193e8b673cd5bbce8f2e
shlkarl <155470542+shlkarl@users.noreply.github.com>
2024-09-23 14:43:57 +0300

    Add files via upload

---
+++ static/js/scr.js
@@ -0,0 +1,30 @@
+﻿function showprod() {
+    var popup = document.getElementById("1");
+    if (popup.style.display === "none") {
+        popup.style.display = "block";
+    } else {
+        popup.style.display = "none";
+    }
+}
+function showus() {
+    var popup = document.getElementById("2");
+    if (popup.style.display === "none") {
+        popup.style.display = "block";
+    } else {
+        popup.style.display = "none";
+    }
+}
+function showser() {
+    var popup = document.getElementById("3");
+    if (popup.style.display === "none") {
+        popup.style.display = "block";
+    } else {
+        popup.style.display = "none";
+    }
+}
+
+function enter() {
+    const popWin = window.open('LK.html', 'LK',
+        'top=150, left=100, width=350, height=100')
+}
+
---
+++ static/js/script.js
@@ -0,0 +1,32 @@
+﻿
+alert('Данный сайт демонстрирует возможности html для создания интернет страниц, некоторые возможности каскадных таблиц стилей css, а так же основы языка javascript');
+
+function showprod() {
+    var popup = document.getElementById("1");
+    if (popup.style.display === "none") {
+        popup.style.display = "block";
+    } else {
+        popup.style.display = "none";
+    }
+}
+function showus() {
+    var popup = document.getElementById("2");
+    if (popup.style.display === "none") {
+        popup.style.display = "block";
+    } else {
+        popup.style.display = "none";
+    }
+}
+function showser() {
+    var popup = document.getElementById("3");
+    if (popup.style.display === "none") {
+        popup.style.display = "block";
+    } else {
+        popup.style.display = "none";
+    }
+}
+function enter() {
+    const popWin = window.open('LK', 'LK',
+        'top=150, left=100, width=750, height=500')
+}
+
